cf6f29de60b6f0c1b1990a2cf2dc36851ed73584
ragnarlaud <ragnar.laud@tptlive>
2016-04-14 17:25:06 +0300

    Edit the Main class to clarify that it is a class for testing

--- src/net/xeprione/http/Main.java
+++ src/net/xeprione/http/Main.java
@@ -1,12 +1,19 @@
 package net.xeprione.http;
 
+/**
+ * This is a test class to show how easy it is to use the library.
+ * It uses the HTTP protocol tester provided by httpbin.org
+ */
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        // Create the header
         HttpHeader header = new HttpHeader(HttpHeader.Method.GET, new RequestURL("http://httpbin.org/ip"));
+        // Print out the header
         System.out.println(header);
-        HttpRequest request = new HttpRequest(header);
-        HttpResponse response = request.execute();
+        // Create a request and execute it
+        HttpResponse response = new HttpRequest(header).execute();
+        // Print out the response
         System.out.println(response);
     }
 }
